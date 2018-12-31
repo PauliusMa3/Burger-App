9db45d956b2e6df981645f54018f7f1a4313f8db
Paulius Malinauskas <paulius.malinauskas@rantandrave.com>
2018-12-31 15:00:30 +0000

    Adding extra files including package.json

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout";
+import "./App.css";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+//import Checkout from "./containers/checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
+//import Orders from "./containers/Orders/Orders";
+// import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/Logout/Logout";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
+import { withRouter } from "react-router";
+import asynchComponent from './hoc/asychComponent/aysnchComponent';
+
+const asychCheckout = asynchComponent(() => {
+  return import("./containers/checkout/Checkout");
+});
+
+const asynchOrders = asynchComponent(() => {
+  return import("./containers/Orders/Orders");
+});
+
+const asychAuth = asynchComponent(() => {
+  return import("./containers/Auth/Auth");
+});
+
 
 class App extends Component {
+
+
+  componentDidMount() {
+    this.props.onTryAutoSignIn();
+  }
+
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={asychAuth} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" component={BurgerBuilder} />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={asychCheckout} />
+          <Route path="/orders" component={asynchOrders} />
+          <Route path="/auth" component={asychAuth} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>
+      );
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Layout>{routes}</Layout>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignIn: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
